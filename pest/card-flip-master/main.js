112f907aec90112042ad7d36da5133fd005c5e69
gslprasanna <63353137+gslprasanna@users.noreply.github.com>
2023-09-05 15:17:33 +0530

    Commit 3

---
+++ pest/card-flip-master/main.js
@@ -0,0 +1,51 @@
+const card1 = document.querySelector(".card__inner");
+
+card1.addEventListener("click", function () {
+  card1.classList.toggle('is-flipped');
+});
+const card2 = document.querySelector(".card__inner2");
+card2.addEventListener("click", function () {
+  card2.classList.toggle('is-flipped2');
+});
+const card3 = document.querySelector(".card__inner3");
+
+card3.addEventListener("click", function () {
+  card3.classList.toggle('is-flipped3');
+});
+const card4 = document.querySelector(".card__inner4");
+
+card4.addEventListener("click", function () {
+  card4.classList.toggle('is-flipped4');
+});
+const card5 = document.querySelector(".card__inner5");
+card5.addEventListener("click", function () {
+  card5.classList.toggle('is-flipped5');
+});
+const card6 = document.querySelector(".card__inner6");
+card6.addEventListener("click", function () {
+  card6.classList.toggle('is-flipped6');
+});
+const card7 = document.querySelector(".card__inner7");
+card7.addEventListener("click", function () {
+  card7.classList.toggle('is-flipped7');
+});
+const card8 = document.querySelector(".card__inner8");
+card8.addEventListener("click", function () {
+  card8.classList.toggle('is-flipped8');
+});
+const card9 = document.querySelector(".card__inner9");
+card9.addEventListener("click", function () {
+  card9.classList.toggle('is-flipped9');
+});
+const card10 = document.querySelector(".card__inner10");
+card10.addEventListener("click", function () {
+  card10.classList.toggle('is-flipped10');
+});
+const card11 = document.querySelector(".card__inner11");
+card11.addEventListener("click", function () {
+  card11.classList.toggle('is-flipped11');
+});
+const card12 = document.querySelector(".card__inner12");
+card12.addEventListener("click", function () {
+  card12.classList.toggle('is-flipped12');
+});
